517fb0cb37f96f6930d1f4d03b2bdda7e90776c3
spasmann <46267220+spasmann@users.noreply.github.com>
2022-06-29 13:08:57 -0600

    Misc

--- scripts/reeds.py
+++ scripts/reeds.py
@@ -4,10 +4,10 @@ sys.path.append(os.getcwd()+"/../")
 from src.init_files.reeds_init import ReedsInit
 #from src.init_files.reeds_solution import reeds_julia_sol, reeds_mcdc_sol, reeds_sol
 from src.solvers.solvers import LGMRES, Picard
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from mpi4py import MPI
 import numpy as np
-#import time
+import time
 import cProfile
 
 if __name__ == "__main__":
@@ -16,17 +16,18 @@ if __name__ == "__main__":
     nproc = comm.Get_size()
     procname = MPI.Get_processor_name()
     
-    N = 2**11
+    N = 2**14
     Nx = 80
     G = 1
     generator = "sobol"
     data = ReedsInit(N=N, Nx=Nx, generator=generator)
-    #start = time.time()
-    maxit = 10
-    phi = LGMRES(data,maxit=maxit, save_data=False)
-    #stop = time.time()
-    #print("time: ",stop-start)
-    #plt.plot(range(Nx),phi)
+    start = time.time()
+    maxit = 20
+    phi = Picard(data,maxit=maxit, save_data=False,report_progress=True)
+    stop = time.time()
+    if (rank==0):
+        print("time: ",stop-start)
+        plt.plot(range(Nx),phi)
 
         
 
