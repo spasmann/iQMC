1cede5c54a5ef289ddf737194b65472e1edfe80b
spasmann <46267220+spasmann@users.noreply.github.com>
2022-11-09 14:11:38 -0700

    wip

--- scripts/U_2_0_SP.py
+++ scripts/U_2_0_SP.py
@@ -8,23 +8,24 @@ Created on Thu Apr  7 13:31:15 2022
 
 import sys
 sys.path.append("../")
-from src.input_files.U_2_0_SL_init import U_2_0_SL_init
+from src.input_files.U_2_0_SP_init import U_2_0_SP_init
 from src.solvers.eigenvalue.solvers import PowerIteration
 from src.solvers.eigenvalue.davidson import Davidson
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # initialize problem data
-    Nx = 20
-    N = 2**10
+    Nx = 9
+    N = 4
+    maxit = 1
     generator = "halton"
     solver = "LGMRES"
-    data = U_2_0_SL_init(N=N, Nx=Nx, generator=generator)
+    data = U_2_0_SP_init(N=N, Nx=Nx, generator=generator)
     data.save_data = False
     phi = PowerIteration(data,
                         solver=solver,
-                        max_outter_itt=10, 
-                        max_inner_itt=10, 
+                        max_outter_itt=maxit, 
+                        max_inner_itt=maxit, 
                         outter_tol=1e-5,
                         inner_tol=1e-5)
     #phi = Davidson(data, tol=1e-5, maxit=10)
